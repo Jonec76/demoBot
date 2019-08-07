ce92cc436333c63f8c1ab069849231265725ffe6
James Wang <jaja076076@gmail.com>
2019-08-07 14:01:53 +0800

    Finish the automatic reply after the user comments the post

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config');
+var axios = require('axios')
 
-/* GET home page. */
+const POST_ID = config.fb.postId
+var access_token = config.fb.token
+var messages = config.messages
+
 router.get('/', function (req, res, next) {
 	if (req.query['hub.verify_token'] === config.fb.webhook) {
 		res.send(req.query['hub.challenge']);
@@ -12,5 +16,30 @@ router.get('/', function (req, res, next) {
 	}
 });
 
+function getMessages(){
+	return messages[Math.floor(Math.random()*messages.length)];
+}
+
+async function reply(comment_id, access_token){
+	let message = getMessages()
+	await axios.post("https://graph.facebook.com/v3.2/" + comment_id + "/comments?access_token=" + access_token,  {message: message}).catch(error => {})
+}
+
+router.post('/', function (req, res) {
+	let body = req.body;
+	body.entry.forEach(function (event) {
+		if (event.hasOwnProperty('changes')) { 
+			let commentBody = event["changes"][0]["value"]
+			console.log(commentBody)
+			if(commentBody.hasOwnProperty('comment_id') && commentBody["post_id"] === POST_ID){// 文章留言
+				comment = event["changes"][0]["value"]["message"]
+				let comment_id = commentBody["comment_id"]
+				reply(comment_id, access_token)
+			}
+		}else if(event.hasOwnProperty('messaging')){
+			// Handle the messaging event
+		}
+	})
+})
 
 module.exports = router;
